ce681a82d54ad1934d61cc6cd66c2eb37b449053
Reavenk <reavenk@gmail.com>
2024-01-02 16:00:10 -0600

    Rework for hover-over callbacks
    Hover-over callbacks added as hooks to change and restore the
    mouse cursor graphic (mouse cursor graphic functionality not
    included in library)

--- Sash.cs
+++ Sash.cs
@@ -7,126 +7,192 @@
 // The sash for the UIUtils.Splitter class.
 // </summary>
 
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-namespace PxPre
+namespace PxPre.UIUtils
 { 
-    namespace UIUtils
+    /// <summary>
+    /// The sash for the UIUtils.Splitter class.
+    /// This should not be authored in the editor, but auto-created by Splitter.
+    /// 
+    /// It's main job is to detect and handle mouse drag operations.
+    /// </summary>
+    public class SplitterSash : 
+        UnityEngine.UI.Image,
+        UnityEngine.EventSystems.IDragHandler,
+        UnityEngine.EventSystems.IBeginDragHandler,
+        UnityEngine.EventSystems.IEndDragHandler,
+        UnityEngine.EventSystems.IPointerEnterHandler,
+        UnityEngine.EventSystems.IPointerExitHandler
     { 
-        /// <summary>
-        /// The sash for the UIUtils.Splitter class.
-        /// 
-        /// It's main job is to detect and handle mouse drag operations.
-        /// </summary>
-        public class SplitterSash : 
-            UnityEngine.UI.Image,
-            UnityEngine.EventSystems.IDragHandler
+        [Flags]
+        public enum State
         { 
-            /// <summary>
-            /// The parent class. Used to get the splitter properties, and to
-            /// signal the parent of changes that occured within the sash.
-            /// </summary>
-            public Splitter parent;
+            Dragging,
+            Hovering
+        }
 
-            /// <summary>
-            /// The left/top pane.
-            /// </summary>
-            public RectTransform paneA;
+        public static Action<SplitterSash, State> OnBeginDrag = null;
+        public static Action<SplitterSash, State> OnEndDrag = null;
+        public static Action<SplitterSash, State> OnHoverEnter = null;
+        public static Action<SplitterSash, State> OnHoverExit = null;
 
-            /// <summary>
-            /// The right/bottom pane.
-            /// </summary>
-            public RectTransform paneB;
+        /// <summary>
+        /// The parent class. Used to get the splitter properties, and to
+        /// signal the parent of changes that occured within the sash.
+        /// </summary>
+        public Splitter parent;
 
-            void UnityEngine.EventSystems.IDragHandler.OnDrag(
-                UnityEngine.EventSystems.PointerEventData eventData)
-            {
-                int vidx = this.parent.GetVectorIndex();
-                float delta = eventData.delta[vidx];
+        /// <summary>
+        /// The left/top pane.
+        /// </summary>
+        public RectTransform paneA;
+
+        /// <summary>
+        /// The right/bottom pane.
+        /// </summary>
+        public RectTransform paneB;
+
+        State state = 0;
+
+        protected override void OnDestroy()
+        {
+            ClearState();
+            base.OnDestroy();
+        }
+
+        protected override void OnDisable()
+        {
+            ClearState();
+            base.OnDisable();
+        }
+
+        void ClearState()
+        { 
+            bool wasHovered = (this.state & State.Hovering) != 0;
+            bool wasDragging = (this.state & State.Dragging) != 0;
+            this.state = 0;
+            if(wasHovered)
+                OnHoverExit?.Invoke(this, this.state);
+            if(wasDragging)
+                OnEndDrag?.Invoke(this, this.state);
+        }
+
+        void UnityEngine.EventSystems.IDragHandler.OnDrag(
+            UnityEngine.EventSystems.PointerEventData eventData)
+        {
+            int vidx = this.parent.GetVectorIndex();
+            float delta = eventData.delta[vidx];
+            if(delta == 0.0f)
+                return;
+
+            float minsize = parent.minSize;
+                
+            float szA = paneA.rect.size[vidx];
+            float szB = paneB.rect.size[vidx];
+
+            // When we do the actual math, we're going to break out in
+            // every single permutation of grain (horiz and vert) and 
+            // direction (positive delta and negative delta).
+            //
+            // Because every case is being explicitly handled, we're not
+            // going to worry about mechanization and indexing, and just
+            // access components explicitly to save some sanity.
+            //
+            // The main thing stopping us from mechanizing this, is that 
+            // I don't also want to throw the sign of the grain (how positive
+            // Y is up, instead of the more convenient down direction) into 
+            // the mix.
+            //
+            //      HORIZONTAL HANDLER
+            //
+            //////////////////////////////////////////////////
+            if (vidx == 0)
+            { 
+                if(delta < 0.0f)
+                { 
+                    float endSize = Mathf.Max(minsize, szA + delta);
+                    delta = endSize - szA;
+                }
+                else
+                {
+                    float endSize = Mathf.Max(minsize, szB - delta);
+                    delta = szB - endSize;
+                }
                 if(delta == 0.0f)
                     return;
 
-                float minsize = parent.minSize;
-                
-                float szA = paneA.rect.size[vidx];
-                float szB = paneB.rect.size[vidx];
+                delta /= this.transform.lossyScale.x;
 
-                // When we do the actual math, we're going to break out in
-                // every single permutation of grain (horiz and vert) and 
-                // direction (positive delta and negative delta).
-                //
-                // Because every case is being explicitly handled, we're not
-                // going to worry about mechanization and indexing, and just
-                // access components explicitly to save some sanity.
-                //
-                // The main thing stopping us from mechanizing this, is that 
-                // I don't also want to throw the sign of the grain (how positive
-                // Y is up, instead of the more convenient down direction) into 
-                // the mix.
-                //
-                //      HORIZONTAL HANDLER
-                //
-                //////////////////////////////////////////////////
-                if (vidx == 0)
+                paneA.sizeDelta = new Vector2(paneA.sizeDelta.x + delta, paneA.sizeDelta.y);
+                paneB.anchoredPosition = new Vector2(paneB.anchoredPosition.x + delta, paneB.anchoredPosition.y);
+                paneB.sizeDelta = new Vector2(paneB.sizeDelta.x - delta, paneB.sizeDelta.y);
+
+                this.rectTransform.anchoredPosition = 
+                    new Vector2(
+                        this.rectTransform.anchoredPosition.x + delta, 
+                        this.rectTransform.anchoredPosition.y);
+            }
+            //      VERTICAL HANDLER
+            //
+            //////////////////////////////////////////////////
+            else
+            { 
+                if(delta < 0.0f)
                 { 
-                    if(delta < 0.0f)
-                    { 
-                        float endSize = Mathf.Max(minsize, szA + delta);
-                        delta = endSize - szA;
-                    }
-                    else
-                    {
-                        float endSize = Mathf.Max(minsize, szB - delta);
-                        delta = szB - endSize;
-                    }
-                    if(delta == 0.0f)
-                        return;
-
-                    paneA.sizeDelta = new Vector2(paneA.sizeDelta.x + delta, paneA.sizeDelta.y);
-                    paneB.anchoredPosition = new Vector2(paneB.anchoredPosition.x + delta, paneB.anchoredPosition.y);
-                    paneB.sizeDelta = new Vector2(paneB.sizeDelta.x - delta, paneB.sizeDelta.y);
-
-                    this.rectTransform.anchoredPosition = 
-                        new Vector2(
-                            this.rectTransform.anchoredPosition.x + delta, 
-                            this.rectTransform.anchoredPosition.y);
+                    float endSize = Mathf.Max(minsize, szB + delta);
+                    delta = endSize - szB;
                 }
-                //      VERTICAL HANDLER
-                //
-                //////////////////////////////////////////////////
                 else
                 { 
-                    if(delta < 0.0f)
-                    { 
-                        float endSize = Mathf.Max(minsize, szB + delta);
-                        delta = endSize - szB;
-                    }
-                    else
-                    { 
-                        float endSize = Mathf.Max(minsize, szA - delta);
-                        delta = szA - endSize;
-                    }
-                    if(delta == 0.0f)
-                        return;
-
-                    paneA.sizeDelta = new Vector2(paneA.sizeDelta.x, paneA.sizeDelta.y - delta);
-                    paneB.anchoredPosition = new Vector2(paneB.anchoredPosition.x, paneB.anchoredPosition.y + delta);
-                    paneB.sizeDelta = new Vector2(paneB.sizeDelta.x, paneB.sizeDelta.y + delta);
-
-                    this.rectTransform.anchoredPosition =
-                        new Vector2(
-                            this.rectTransform.anchoredPosition.x,
-                            this.rectTransform.anchoredPosition.y + delta);
+                    float endSize = Mathf.Max(minsize, szA - delta);
+                    delta = szA - endSize;
                 }
+                if(delta == 0.0f)
+                    return;
 
-                // If we've gotten this far, something was modified
-                // We need to update the individual records for the next 
-                // time a full update is done.
-                this.parent.UpdateSize(paneA);
-                this.parent.UpdateSize(paneB);
+                delta /= this.transform.lossyScale.y;
+
+                paneA.sizeDelta = new Vector2(paneA.sizeDelta.x, paneA.sizeDelta.y - delta);
+                paneB.anchoredPosition = new Vector2(paneB.anchoredPosition.x, paneB.anchoredPosition.y + delta);
+                paneB.sizeDelta = new Vector2(paneB.sizeDelta.x, paneB.sizeDelta.y + delta);
+
+                this.rectTransform.anchoredPosition =
+                    new Vector2(
+                        this.rectTransform.anchoredPosition.x,
+                        this.rectTransform.anchoredPosition.y + delta);
             }
+
+            // If we've gotten this far, something was modified
+            // We need to update the individual records for the next 
+            // time a full update is done.
+            this.parent.UpdateSize(paneA);
+            this.parent.UpdateSize(paneB);
+        }
+
+        void UnityEngine.EventSystems.IBeginDragHandler.OnBeginDrag(UnityEngine.EventSystems.PointerEventData eventData)
+        { 
+            this.state |= State.Dragging;
+            OnBeginDrag?.Invoke(this, this.state);
+        }
+
+        void UnityEngine.EventSystems.IEndDragHandler.OnEndDrag(UnityEngine.EventSystems.PointerEventData eventData)
+        { 
+            this.state &= ~State.Dragging;
+            OnEndDrag?.Invoke(this, this.state);
+        }
+
+        void UnityEngine.EventSystems.IPointerEnterHandler.OnPointerEnter(UnityEngine.EventSystems.PointerEventData eventData)
+        {
+            this.state |= State.Hovering;
+            OnHoverEnter?.Invoke(this, this.state);
+        }
+
+        void UnityEngine.EventSystems.IPointerExitHandler.OnPointerExit(UnityEngine.EventSystems.PointerEventData eventData)
+        { 
+            this.state &= ~State.Hovering;
+            OnHoverExit?.Invoke(this, this.state);
         }
     }
 }
--- SplitterProps.cs
+++ SplitterProps.cs
@@ -12,38 +12,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace PxPre
+namespace PxPre.UIUtils
 {
-    namespace UIUtils
+    /// <summary>
+    /// The splitter aethetics and behaviour properties collection
+    /// for the UIUtils.Splitter class.
+    /// </summary>
+    [CreateAssetMenu(menuName = "PxPre/SplitterProps")]
+    public class SplitterProps : ScriptableObject
     {
         /// <summary>
-        /// The splitter aethetics and behaviour properties collection
-        /// for the UIUtils.Splitter class.
+        /// The sprite to use for horizontal splitters.
         /// </summary>
-        [CreateAssetMenu(menuName = "PxPre/SplitterProps")]
-        public class SplitterProps : ScriptableObject
-        {
-            /// <summary>
-            /// The sprite to use for horizontal splitters.
-            /// </summary>
-            public Sprite spriteHoriz;
+        public Sprite spriteHoriz;
 
-            /// <summary>
-            /// The sprite to use for vertical splitters.
-            /// </summary>
-            public Sprite spriteVert;  // Only applies to things split vertically
+        /// <summary>
+        /// The sprite to use for vertical splitters.
+        /// </summary>
+        public Sprite spriteVert;  // Only applies to things split vertically
 
-            /// <summary>
-            /// The dimensions of the sash. The x component is the size for horizontal
-            /// splitters, and the y component is the size for vertical spliters. The x
-            /// and the y components will be the same more-often-than-not.
-            /// </summary>
-            public Vector2 sashDim = new Vector2(10.0f, 10.0f);
+        /// <summary>
+        /// Optional sprite that appears in the middle of the thumb when split 
+        /// horizontally
+        /// </summary>
+        public Sprite spriteThumbHoriz;
 
-            /// <summary>
-            /// The modulating color of the sashes.
-            /// </summary>
-            public Color sashColor = Color.white;
-        }
+        /// <summary>
+        /// Optional sprite that appears in the middle of the thumb when split 
+        /// vertically.
+        /// </summary>
+        public Sprite spriteThumbVert;
+
+        public Color thumbSpriteColor = Color.white;
+
+        /// <summary>
+        /// The dimensions of the sash. The x component is the size for horizontal
+        /// splitters, and the y component is the size for vertical spliters. The x
+        /// and the y components will be the same more-often-than-not.
+        /// </summary>
+        public Vector2 sashDim = new Vector2(10.0f, 10.0f);
+
+        /// <summary>
+        /// The modulating color of the sashes.
+        /// </summary>
+        public Color sashColor = Color.white;
     }
 }
